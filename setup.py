a02237c6a9696f96217f7a62aac3d91d455e9837
nick <nickchen1998@gmail.com>
2024-08-28 21:09:22 +0800

    程式: 更新專案的元數據和版本號至0.2.0
    - 在 pyproject.toml 和 setup.py 中將版本號從 "0.1.0" 更新至 "0.2.0"
    - 更改 pyproject.toml 中作者的電子郵件地址
    - 在 pyproject.toml 中新增許可證和首頁 URL
    - 將 setup.py 中的 url 欄位更新為 "https://github.com/nickchen1998/DataToWord"
    
    Signed-off-by: nick <nickchen1998@gmail.com>

--- setup.py
+++ setup.py
@@ -15,14 +15,14 @@ install_requires = \
 
 setup_kwargs = {
     'name': 'datatoword',
-    'version': '0.1.0',
+    'version': '0.2.0',
     'description': '',
     'long_description': '# DataToWord\n\n## 專案目的\n\n這是一個將資料轉換成文字的專案，可以協助你在進行 RAG 的過程中，將資料類型的檔案轉換成文字，方便後續進行段落查詢。\n\n目前支援的檔案有：\n\n- CSV\n- JSON\n\n## 使用方式\n\n### 安裝\n\n```bash\npip install datatoword\n```\nor\n\n```bash\npoetry add datatoword\n```\n\n### Quick Start\n\n```python\n```\n\n## poetry export requirements.txt\n\n```bash\npoetry export -f requirements.txt --output requirements.txt --without-hashes\n```',
     'author': 'nick',
     'author_email': 'nickchen1998@gmail.com',
     'maintainer': 'None',
     'maintainer_email': 'None',
-    'url': 'None',
+    'url': 'https://github.com/nickchen1998/DataToWord',
     'packages': packages,
     'package_data': package_data,
     'install_requires': install_requires,
